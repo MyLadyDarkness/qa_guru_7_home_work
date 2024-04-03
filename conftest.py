f25c7bcb6ece5bf51ba83473dd5a8da8d418e213
Olesya B <OBarkar@it-one.ru>
2024-04-03 13:21:05 +0300

    Fix DIR_PATH

--- conftest.py
+++ conftest.py
@@ -1,24 +1,27 @@
 import os
 import zipfile
+
 import pytest
 
-files = ["Python Notes.pdf", "example.xlsx", "example3.csv"]
+ROOT_PATH = os.getcwd()
+TMP = os.path.join(ROOT_PATH, "temp")
+RES = os.path.join(ROOT_PATH, "resource")
+ARCH = os.path.join(RES, "archive.zip")
 
 
 @pytest.fixture(scope="session", autouse=True)
 def archive_creation():
     files_list = ["Python Notes.pdf", "example.xlsx", "example3.csv"]
-    if not os.path.exists("resource"):
-        os.mkdir("resource")
-        with zipfile.ZipFile("resource/archive.zip", "a") as arch:
-            os.chdir("temp")
-            for file in files_list:
-                arch.write(file)
-    os.chdir("resource")
+    if not os.path.exists(RES):
+        os.mkdir(RES)
+    with zipfile.ZipFile(f"{RES}/archive.zip", "a") as arch:
+        os.chdir(TMP)
+        for file in files_list:
+            arch.write(file)
 
     yield
 
-    if os.path.exists("resource/archive.zip"):
-        os.remove("resource/archive.zip")
-    if os.path.exists("resource"):
-        os.rmdir("resource")
+    if os.path.exists(ARCH):
+        os.remove(ARCH)
+    if os.path.exists(RES):
+        os.rmdir(RES)
--- test_files.py
+++ test_files.py
@@ -1,28 +1,29 @@
 from zipfile import ZipFile
+
 import pypdf
 from openpyxl import load_workbook
 
+from conftest import ARCH
+
 
 def test_pdf():
-    with ZipFile("archive.zip") as zip_pdf:
+    with ZipFile(ARCH) as zip_pdf:
         with zip_pdf.open("Python Notes.pdf") as ext_pdf:
             reader = pypdf.PdfReader(ext_pdf)
             pdf_content = reader.pages[1].extract_text()
-
     assert "If - flow control" in pdf_content
 
 
 def test_xlsx():
-    with ZipFile("archive.zip") as zip_xlsx:
+    with ZipFile(ARCH) as zip_xlsx:
         with zip_xlsx.open("example.xlsx") as example_xlsx:
             reader = load_workbook(example_xlsx)
             xls_file = reader.active
-
     assert "Some" in xls_file.cell(row=1, column=1).value
 
 
 def test_csv():
-    with ZipFile("archive.zip") as zip_csv:
+    with ZipFile(ARCH) as zip_csv:
         with zip_csv.open("example3.csv") as example3_csv:
             csv_content = example3_csv.read().decode('utf-8')
     assert "work3" in csv_content
